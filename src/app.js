897f18f91893df263118df4a89fcf67856822c5e
emilianobrazzoli <emiliano.brazzoli@gmail.com>
2022-02-06 11:41:45 +0100

    pull

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 /** MAIN */
 const express = require('express'); 
 const app = express();  
+var fs = require('fs');
 
 //INIT .ENV VAR
 if(!process || !process.env || !process.env.TOKEN){
@@ -13,27 +14,7 @@ var token = process.env.TOKEN || '';
 app.use(express.static(__dirname + '/dist/'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
-app.get("/", (request, response) => { 
-    response.sendFile('home.html' , { root: './src/resource/html/' })
-});  
-app.get("/EGO", (request, response) => { 
-    response.sendFile('ego.html' , { root: './src/resource/html/' })
-});  
-app.get("/ISTERIA", (request, response) => { 
-    response.sendFile('isteria.html' , { root: './src/resource/html/' })
-});  
-app.get("/SUPEREGO", (request, response) => { 
-    response.sendFile('superego.html' , { root: './src/resource/html/' })
-});  
-app.get("/SOGNATORE", (request, response) => { 
-    response.sendFile('sognatore.html' , { root: './src/resource/html/' })
-});  
-app.get("/AVVERTENZE", (request, response) => { 
-    response.sendFile('avvertenze.html' , { root: './src/resource/html/' })
-});  
-app.get("/IFO", (request, response) => { 
-    response.sendFile('info.html' , { root: './src/resource/html/' })
-});  
+
 app.get("/text.js", (request, response) => { 
     response.sendFile('text.js' , { root: './src/resource/js/' })
 });  
@@ -49,4 +30,13 @@ app.get("/text.scss", (request, response) => {
 app.get("/APPUNTI", (request, response) => { 
     response.sendFile('charity-bolla.html' , { root: './src/resource/html/' }) 
 });  
+
+var files = fs.readdirSync('./src/resource/html/');
+files.forEach(file => { 
+    var streetaddress= file.substr(0, file.indexOf('.')); 
+    app.get("/"+streetaddress, (request, response) => { 
+        response.sendFile(file , { root: './src/resource/html/' }) 
+    });  
+});
+
 app.listen(port);  
